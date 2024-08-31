7746125f2b928bcd1d89ce6686b0716fa67ee7b3
Yeasin Arafat <yeasinn2002@gmail.com>
2024-08-31 23:00:42 +0600

    linting and prettier config setup complete

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["**/node_modules/", ".dist/"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: "readonly",
+      },
+    },
+
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": 0,
+      "no-undef": "error",
+      "@typescript-eslint/no-explicit-any": 1,
+    },
+  },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
