3647cafe4210481a8a7f41594f3890c775e20df9
Yeasin Arafat <yeasinn2002@gmail.com>
2024-09-02 05:46:44 +0600

    request validation function updated

--- dist/app/utils/catchAsync.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const catchAsync = (func) => {
-    return (req, res, next) => {
-        Promise.resolve(func(req, res, next)).catch((err) => next(err));
-    };
-};
-exports.default = catchAsync;
--- dist/app/utils/tesRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TestRoutes = void 0;
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get("/", (req, res) => {
-    res.json({ ok: true });
-});
-exports.TestRoutes = router;
