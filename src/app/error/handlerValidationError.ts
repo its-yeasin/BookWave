56a1d36ce1bffc19f16eb965f272c40fa1ab9a33
Yeasin Arafat <yeasinn2002@gmail.com>
2024-09-02 07:14:46 +0600

    Validaton error handler added

---
+++ src/app/error/handlerValidationError.ts
@@ -0,0 +1,31 @@
+import { TErrorSource } from "../interface/error";
+import mongoose from "mongoose";
+
+type TError = {
+  statusCode: number;
+  message: string;
+  errorSources: TErrorSource;
+};
+
+const handlerValidationError = (
+  err: mongoose.Error.ValidationError
+): TError => {
+  const statusCode = 400;
+  const message = err.message || "Validation error";
+  const errorSources: TErrorSource = Object.values(err.errors)?.map(
+    (vError: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
+      return {
+        path: vError.path,
+        message: vError.message,
+      };
+    }
+  );
+
+  return {
+    statusCode,
+    message,
+    errorSources,
+  };
+};
+
+export default handlerValidationError;
