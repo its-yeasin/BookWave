28ff6e00e558364ded3b9d84c8b1b2e52421ce75
Yeasin Arafat <yeasinn2002@gmail.com>
2024-09-02 07:14:30 +0600

    notFound middleware response pattern updated

--- src/app/middleware/notFound.ts
+++ src/app/middleware/notFound.ts
@@ -1,14 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
-import sendResponse from "../utils/sendResponse";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-  return sendResponse(res, {
+  return res.status(httpStatus.NOT_FOUND).json({
     statusCode: httpStatus.NOT_FOUND,
     success: false,
     message: "API not found",
-    data: null,
   });
 };
 
