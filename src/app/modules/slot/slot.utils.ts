0531ba1cf8ca71c75e28d51a0dcc10514c930925
Yeasin Arafat <yeasinn2002@gmail.com>
2024-09-13 21:19:38 +0600

    slots utils updated

--- src/app/modules/slot/slot.utils.ts
+++ src/app/modules/slot/slot.utils.ts
@@ -8,8 +8,12 @@ export const timeToMinutes = (time: string) => {
 };
 
 export const convertTimeToTimestamp = (time: string) => {
-  const timestamp = new Date(`1970-01-01T${time}:00`);
+  const timestamp = new Date(`1970-01-01T${time}:00`).getTime() / 1000;
 
   // returns timestamp in seconds
   return timestamp;
+};
+
+export const formatTime = (time: number | string) => {
+  return time?.toString()?.padStart(2, "0");
 };
