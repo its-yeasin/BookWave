e0ba21f958ea4ee98334f6c198e35eec2e514497
Yeasin Arafat <yeasinn2002@gmail.com>
2024-09-16 21:00:09 +0600

    Room model updated

--- src/app/modules/room/room.model.ts
+++ src/app/modules/room/room.model.ts
@@ -33,20 +33,9 @@ const roomSchema = new mongoose.Schema<TRoom, RoomModel>(
     },
   },
   {
-    timestamps: true,
+    versionKey: false,
   }
 );
-
-// roomSchema.pre("findOneAndUpdate", async function (next) {
-//   const query = this.getQuery();
-//   const existingRoom: TRoom | null = await Room.findById(query);
-
-//   if (!existingRoom || existingRoom.isDeleted) {
-//     throw new AppError(httpStatus.NOT_FOUND, "Room doest not exist!");
-//   }
-
-//   next();
-// });
 
 roomSchema.static("isDeleted", async function (id: string) {
   const existingRoom: TRoom | null = await Room.findById(id);
