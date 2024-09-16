3a36632142394e0a942aa7ff244d5fd950c108cc
Yeasin Arafat <yeasinn2002@gmail.com>
2024-09-16 21:07:40 +0600

    auth middleware added on room routes

--- src/app/modules/room/room.route.ts
+++ src/app/modules/room/room.route.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import validateRequest from "../../middleware/validateRequest";
 import { RoomControllers } from "./room.controller";
 import { RoomValidations } from "./room.validation";
+import { auth } from "../../middleware/auth";
+import { USER_ROLE } from "../user/user.constants";
 
 const router = Router();
 
 // --Create Room
 router.post(
   "/",
+  auth(USER_ROLE.admin),
   validateRequest(RoomValidations.createRoomValidationSchema),
   RoomControllers.createRoom
 );
@@ -21,11 +24,12 @@ router.get("/:id", RoomControllers.getSingleRoom);
 // --Update Room
 router.put(
   "/:id",
+  auth(USER_ROLE.admin),
   validateRequest(RoomValidations.updateRoomValidationSchema),
   RoomControllers.updateRoom
 );
 
 // --Delete Room
-router.delete("/:id", RoomControllers.deleteRoom);
+router.delete("/:id", auth(USER_ROLE.admin), RoomControllers.deleteRoom);
 
 export const RoomRoutes = router;
